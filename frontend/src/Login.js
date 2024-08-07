cb365ddcbf1e24c52cf1cab12041932ae63c8e77
Bruno Oliveira <brunooliveira.unb@gmail.com>
2024-08-07 02:18:51 -0300

    estilização da página de login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,32 +1,43 @@
 import React from "react";
 import "./index.css";
+import Logo from "./assets/Logo.png";
 
 function Login() {
   return (
     <div className="d-flex justify-content-center align-items-center bg-white vh-100">
-      <div className="shadow bg-light p-5 rounded w-40">
+      <div className="shadow bg-light px-5 pb-5 rounded w-40">
+        <img alt="..." src={Logo} style={{ width: "340px" }} />
         <form action="">
           <div className="mb-4">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
             <input
               type="email"
               placeholder="Enter Email"
-              className="form-control rounded-0"
+              className="form-control rounded-2 shadow-sm"
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
             <input
               type="password"
               placeholder="Enter Password"
-              className="form-control rounded-0"
+              className="form-control rounded-2 shadow-sm"
             />
           </div>
-          <button className="btn btn-success w-100">Log in</button>
-          <p>You are agree to our terms and policies?</p>
-          <button className="btn btn-default border w-100">
-            Create Account
+          <button className="btn btn-dark w-100 rounded-2 mt-4 shadow-sm">
+            Log in
           </button>
+          <p className="mt-4">
+            Don't have an account?{" "}
+            <strong style={{ color: "#9667e0", marginLeft: "4px" }}>
+              {" "}
+              Sign up
+            </strong>
+          </p>
         </form>
       </div>
     </div>
