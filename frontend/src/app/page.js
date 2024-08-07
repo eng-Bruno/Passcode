3b58ad211616573bd71a4505074c6b37bf414247
Bruno Oliveira <brunooliveira.unb@gmail.com>
2024-08-07 00:49:23 -0300

    migrando para Reactjs

--- frontend/src/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center">
-      <div className="flex items-center justify-center">
-        <h1 className="text-center">PASSCODE</h1>
-      </div>
-    </div>
-  );
-}
--- frontend/src/app/users/register/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <div>
-        <h1 className="text-center">Register</h1>
-      </div>
-    </div>
-  );
-}
